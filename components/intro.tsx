b170e623852e1aed4d8d5ad2865e1578158e17db
cvhlpont <pontchvl@gmail.com>
2024-10-18 00:23:37 +0200

    updated correct cv

--- components/intro.tsx
+++ components/intro.tsx
@@ -92,7 +92,7 @@ const Intro = () => {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack"
-          href="/CV.pdf"
+          href="/cvpontushogler.pdf"
           download
         >
           Download CV{" "}
